153519201e24b0c72565e8c91c75ac6f57d96542
vyasinskiy <yasinsky93@gmail.com>
2022-12-28 15:46:58 +0400

    Ad styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { useSelector } from 'react-redux';
 import * as React from 'react';
+import { useState, FormEvent, ChangeEvent } from 'react';
+import { useSelector } from 'react-redux';
 import { useAppDispatch, setAsSucceed, RootState } from './store';
-import { CheckForm } from './components/CheckForm';
-import { useState } from 'react';
+import { Button, TextField } from '@mui/material';
+
+import styles from './App.module.scss';
 
 function App() {
-	const { all } = useSelector((state: RootState) => state);
+	const dispatch = useAppDispatch();
 	const [currentIndex, setCurrentIndex] = useState(0);
+
+	const [value, setValue] = useState('');
 	const [error, setError] = useState('');
 
-	const dispatch = useAppDispatch();
+	const { all } = useSelector((state: RootState) => state);
 
-	const handleCheck = (value: string) => {
+	const onChange = (event: ChangeEvent<HTMLInputElement>) =>
+		setValue(event.target.value);
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
 		const areEqual = all[currentIndex]['Search text'] === value;
 
 		if (!areEqual) {
@@ -23,11 +31,33 @@ function App() {
 		setCurrentIndex((index) => ++index);
 	};
 
+	const taskWord = all[currentIndex]['Translation text'];
+
 	return (
-		<div className="App">
-			Переведите слово: {all[currentIndex]['Search text']}
-			<CheckForm onCheck={handleCheck} />
-			{error}
+		<div className={styles.wrapper}>
+			<h1>{taskWord[0].toUpperCase() + taskWord.slice(1)}</h1>
+
+			<form onSubmit={handleSubmit}>
+				<TextField
+					className={styles.verticalShift}
+					color="primary"
+					onChange={onChange}
+					value={value}
+					fullWidth={true}
+					label="Type your answer"
+					variant="outlined"
+					error={Boolean(error)}
+					helperText={error}
+				/>
+				<Button
+					className={styles.verticalShift}
+					variant="contained"
+					fullWidth={true}
+					type="submit"
+				>
+					Answer
+				</Button>
+			</form>
 		</div>
 	);
 }
